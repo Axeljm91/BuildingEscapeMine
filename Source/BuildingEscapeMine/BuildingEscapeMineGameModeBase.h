ab3c0fd9095175f192babe7b490d37edeccc6951
Axeljm91 <37466760+Axeljm91@users.noreply.github.com>
2018-03-21 15:27:35 -0500

    Initial commit

---
+++ Source/BuildingEscapeMine/BuildingEscapeMineGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapeMineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEMINE_API ABuildingEscapeMineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
